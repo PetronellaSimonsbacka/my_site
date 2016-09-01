3c4fb519889461585593471a0c923f08caead3ba
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-01 11:23:21 +0200

    Getting good results

--- lib/data.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-class Prep
-
-  attr_accessor :prep, :name, :description, :url
-
-  def initialize
-    @prep = YAML.load_file('./data/prep.yml')
-  end
--- lib/icon_helper.rb
+++
@@ -1,8 +0,0 @@
-module IconHelper
-  def get_icon(type)
-      case type
-      when 'LinkedIn' then 'fi-social-linkedin'
-      when 'GitHub' then 'fi-social-github'
-      end
-  end
-end
